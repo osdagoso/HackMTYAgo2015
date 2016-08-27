06ccd725c3280bb19e05969a449d79a530e50805
Andres Altamirano Montealvo <Andres Altamirano Montealvo>
2016-08-27 12:24:47 -0500

    Added TextToSpeech

---
+++ TextToSpeech/app/src/main/java/com/aam/texttospeech/MainActivity.java
@@ -0,0 +1,90 @@
+package com.aam.texttospeech;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener, OnInitListener {
+
+    private int MY_DATA_CHECK_CODE = 0;
+    private TextToSpeech myTTS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent checkTTSIntent = new Intent();
+        checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkTTSIntent, MY_DATA_CHECK_CODE);
+
+        Button speakButton = (Button)findViewById(R.id.speak);
+        speakButton.setOnClickListener(this);
+    }
+
+    public void onInit(int initStatus) {
+        if (initStatus == TextToSpeech.SUCCESS) {
+            myTTS.setLanguage(Locale.US);
+        }
+        else if (initStatus == TextToSpeech.ERROR) {
+            Toast.makeText(this, "Sorry! Text To Speech failed...", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MY_DATA_CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                myTTS = new TextToSpeech(this, this);
+            }
+            else {
+                Intent installTTSIntent = new Intent();
+                installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installTTSIntent);
+            }
+        }
+    }
+
+    public void onClick(View v) {
+        EditText enteredText = (EditText)findViewById(R.id.enter);
+        String words = enteredText.getText().toString();
+        if (words.length() > 0) {
+            speakWords(words);
+        }
+        else {
+            Toast.makeText(this, "The string is empty!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void speakWords(String speech) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ttsGreater21(speech);
+        } else {
+            ttsUnder20(speech);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private void ttsUnder20(String text) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
+        myTTS.speak(text, TextToSpeech.QUEUE_FLUSH, map);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void ttsGreater21(String text) {
+        String utteranceId=this.hashCode() + "";
+        myTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+    }
+}
