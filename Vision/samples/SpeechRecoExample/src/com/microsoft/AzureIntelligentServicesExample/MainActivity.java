34e864402d288c58d2f8b291886d2e3704291118
Andres Altamirano Montealvo <Andres Altamirano Montealvo>
2016-08-27 23:12:36 -0500

    Added Vision

---
+++ Vision/samples/SpeechRecoExample/src/com/microsoft/AzureIntelligentServicesExample/MainActivity.java
@@ -0,0 +1,327 @@
+/*
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license.
+ * //
+ * Microsoft Cognitive Services (formerly Project Oxford): https://www.microsoft.com/cognitive-services
+ * //
+ * Microsoft Cognitive Services (formerly Project Oxford) GitHub:
+ * https://github.com/Microsoft/Cognitive-Speech-STT-Android
+ * //
+ * Copyright (c) Microsoft Corporation
+ * All rights reserved.
+ * //
+ * MIT License:
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * //
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * //
+ * THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.microsoft.AzureIntelligentServicesExample;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.widget.TextView;
+
+import com.microsoft.projectoxford.speechrecognition.DataRecognitionClient;
+import com.microsoft.projectoxford.speechrecognition.ISpeechRecognitionServerEvents;
+import com.microsoft.projectoxford.speechrecognition.MicrophoneRecognitionClient;
+import com.microsoft.projectoxford.speechrecognition.RecognitionResult;
+import com.microsoft.projectoxford.speechrecognition.SpeechRecognitionMode;
+import com.microsoft.projectoxford.speechrecognition.SpeechRecognitionServiceFactory;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Locale;
+
+public class MainActivity extends Activity implements ISpeechRecognitionServerEvents, TextToSpeech.OnInitListener
+{
+    private int MY_DATA_CHECK_CODE = 0;
+    private TextToSpeech myTTS;
+
+    DataRecognitionClient dataClient = null;
+    MicrophoneRecognitionClient micClient = null;
+    FinalResponseStatus isReceivedResponse = FinalResponseStatus.NotReceived;
+
+    TextView mTextView;
+
+    public enum FinalResponseStatus { NotReceived, OK, Timeout }
+
+    /**
+     * Gets the primary subscription key
+     */
+    public String getPrimaryKey() {
+        return this.getString(R.string.primaryKey);
+    }
+
+    /**
+     * Gets the secondary subscription key
+     */
+    public String getSecondaryKey() {
+        return this.getString(R.string.secondaryKey);
+    }
+
+    /**
+     * Gets the LUIS application identifier.
+     * @return The LUIS application identifier.
+     */
+    private String getLuisAppId() {
+        return this.getString(R.string.luisAppID);
+    }
+
+    /**
+     * Gets the LUIS subscription identifier.
+     * @return The LUIS subscription identifier.
+     */
+    private String getLuisSubscriptionID() {
+        return this.getString(R.string.luisSubscriptionID);
+    }
+
+    /**
+     * Gets the default locale.
+     * @return The default locale.
+     */
+    private String getDefaultLocale() {
+        return "en-us";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.mainText);
+
+        Intent checkTTSIntent = new Intent();
+        checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkTTSIntent, MY_DATA_CHECK_CODE);
+
+        if (getString(R.string.primaryKey).startsWith("Please")) {
+            new AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.add_subscription_key_tip_title))
+                    .setMessage(getString(R.string.add_subscription_key_tip))
+                    .setCancelable(false)
+                    .show();
+        }
+
+        final MainActivity This = this;
+        This.StartListening();
+    }
+
+    public void onInit(int initStatus) {
+        if (initStatus == TextToSpeech.SUCCESS) {
+            myTTS.setLanguage(Locale.US);
+
+            myTTS.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+                @Override
+                public void onStart(String s) {
+                }
+
+                @Override
+                public void onDone(String s) {
+                    micClient.startMicAndRecognition();
+                }
+
+                @Override
+                public void onError(String s) {
+                }
+            });
+        }
+        else if (initStatus == TextToSpeech.ERROR) {
+            // do nothing
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MY_DATA_CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                myTTS = new TextToSpeech(this, this);
+            }
+            else {
+                Intent installTTSIntent = new Intent();
+                installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installTTSIntent);
+            }
+        }
+    }
+
+    public void Speak(String words) {
+        if (words.length() > 0) {
+            speakWords(words);
+        }
+    }
+
+    private void speakWords(String speech) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ttsGreater21(speech);
+        } else {
+            ttsUnder20(speech);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private void ttsUnder20(String text) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
+        myTTS.speak(text, TextToSpeech.QUEUE_FLUSH, map);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void ttsGreater21(String text) {
+        String utteranceId = this.hashCode() + "";
+        myTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+    }
+
+    /**
+     * Handles the event of starting the microphone.
+     */
+    private void StartListening() {
+
+        while (this.micClient == null) {
+            this.micClient = SpeechRecognitionServiceFactory.createMicrophoneClientWithIntent(
+                    this,
+                    this.getDefaultLocale(),
+                    this,
+                    this.getPrimaryKey(),
+                    this.getSecondaryKey(),
+                    this.getLuisAppId(),
+                    this.getLuisSubscriptionID());
+        }
+
+        this.micClient.startMicAndRecognition();
+    }
+
+    public void onFinalResponseReceived(final RecognitionResult response) {
+        if (this.micClient != null) {
+            // we got the final result, so it we can end the mic reco.  No need to do this
+            // for dataReco, since we already called endAudio() on it as soon as we were done
+            // sending all the data.
+            this.micClient.endMicAndRecognition();
+        }
+
+        String instruction = "";
+        if (response.Results.length > 0) {
+            instruction = response.Results[0].DisplayText;
+        }
+
+        if (instruction.length() > 0) {
+            instruction = instruction.substring(0, instruction.length() - 1);
+            if (instruction.equals("Take a photo") ||
+                    instruction.equals("Take a picture") ||
+                    instruction.equals("Take photo") ||
+                    instruction.equals("Take picture")) {
+                Speak("Taking photos");
+            }
+            else if(instruction.equals("Help")){
+                Speak("App can help you familiarize with your surroundings. Say take a photo to snap a picture and get information about it or say help to hear this again.");
+            }
+            else {
+                Speak("I'm sorry. I didn't get that");
+            }
+        } else {
+            StartListening();
+        }
+
+        mTextView.setText(instruction);
+
+    }
+
+    /**
+     * Called when a final response is received and its intent is parsed
+     */
+    public void onIntentReceived(final String payload) {
+        // Empty. We don't need the payload, but we need this method to be declared.
+    }
+
+    public void onPartialResponseReceived(final String response) {
+        // Empty. We don't need partial responses, but we need this method to be declared.
+    }
+
+    public void onError(final int errorCode, final String response) {
+        // do nothing
+    }
+
+    /**
+     * Called when the microphone status has changed.
+     * @param recording The current recording state
+     */
+    public void onAudioEvent(boolean recording) {
+        // do nothing. mic status never changes, but we need this method to be declared.
+    }
+
+    /*
+     * Speech recognition with data (for example from a file or audio source).
+     * The data is broken up into buffers and each buffer is sent to the Speech Recognition Service.
+     * No modification is done to the buffers, so the user can apply their
+     * own VAD (Voice Activation Detection) or Silence Detection
+     *
+     * @param dataClient
+     * @param recoMode
+     * @param filename
+     */
+    private class RecognitionTask extends AsyncTask<Void, Void, Void> {
+        DataRecognitionClient dataClient;
+        SpeechRecognitionMode recoMode;
+        String filename;
+
+        RecognitionTask(DataRecognitionClient dataClient, SpeechRecognitionMode recoMode, String filename) {
+            this.dataClient = dataClient;
+            this.recoMode = recoMode;
+            this.filename = filename;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // Note for wave files, we can just send data from the file right to the server.
+                // In the case you are not an audio file in wave format, and instead you have just
+                // raw data (for example audio coming over bluetooth), then before sending up any
+                // audio data, you must first send up an SpeechAudioFormat descriptor to describe
+                // the layout and format of your raw audio data via DataRecognitionClient's sendAudioFormat() method.
+
+                String filename = recoMode == SpeechRecognitionMode.ShortPhrase ? "whatstheweatherlike.wav" : "batman.wav";
+                InputStream fileStream = getAssets().open(filename);
+                int bytesRead = 0;
+                byte[] buffer = new byte[1024];
+
+                do {
+                    // Get  Audio data to send into byte buffer.
+                    bytesRead = fileStream.read(buffer);
+
+                    if (bytesRead > -1) {
+                        // Send of audio data to service.
+                        dataClient.sendAudio(buffer, bytesRead);
+                    }
+                } while (bytesRead > 0);
+
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+            finally {
+                dataClient.endAudio();
+            }
+
+            return null;
+        }
+    }
+}
